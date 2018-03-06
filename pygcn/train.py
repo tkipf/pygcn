49c94006bf376c88398b7939506a8c33dc61ede8
rusty1s <matthias.fey@tu-dortmund.de>
2018-03-06 14:30:15 +0100

    reformat

--- pygcn/train.py
+++ pygcn/train.py
@@ -15,33 +15,21 @@ from pygcn.models import GCN
 
 # Training settings
 parser = argparse.ArgumentParser()
-parser.add_argument(
-    '--no-cuda',
-    action='store_true',
-    default=False,
-    help='Disables CUDA training.')
-parser.add_argument(
-    '--fastmode',
-    action='store_true',
-    default=False,
-    help='Validate during training pass.')
+parser.add_argument('--no-cuda', action='store_true', default=False,
+                    help='Disables CUDA training.')
+parser.add_argument('--fastmode', action='store_true', default=False,
+                    help='Validate during training pass.')
 parser.add_argument('--seed', type=int, default=42, help='Random seed.')
-parser.add_argument(
-    '--epochs', type=int, default=200, help='Number of epochs to train.')
-parser.add_argument(
-    '--lr', type=float, default=0.01, help='Initial learning rate.')
-parser.add_argument(
-    '--weight_decay',
-    type=float,
-    default=5e-4,
-    help='Weight decay (L2 loss on parameters).')
-parser.add_argument(
-    '--hidden', type=int, default=16, help='Number of hidden units.')
-parser.add_argument(
-    '--dropout',
-    type=float,
-    default=0.5,
-    help='Dropout rate (1 - keep probability).')
+parser.add_argument('--epochs', type=int, default=200,
+                    help='Number of epochs to train.')
+parser.add_argument('--lr', type=float, default=0.01,
+                    help='Initial learning rate.')
+parser.add_argument('--weight_decay', type=float, default=5e-4,
+                    help='Weight decay (L2 loss on parameters).')
+parser.add_argument('--hidden', type=int, default=16,
+                    help='Number of hidden units.')
+parser.add_argument('--dropout', type=float, default=0.5,
+                    help='Dropout rate (1 - keep probability).')
 
 args = parser.parse_args()
 args.cuda = not args.no_cuda and torch.cuda.is_available()
@@ -55,13 +43,12 @@ if args.cuda:
 adj, features, labels, idx_train, idx_val, idx_test = load_data()
 
 # Model and optimizer
-model = GCN(
-    nfeat=features.shape[1],
-    nhid=args.hidden,
-    nclass=labels.max() + 1,
-    dropout=args.dropout)
-optimizer = optim.Adam(
-    model.parameters(), lr=args.lr, weight_decay=args.weight_decay)
+model = GCN(nfeat=features.shape[1],
+            nhid=args.hidden,
+            nclass=labels.max() + 1,
+            dropout=args.dropout)
+optimizer = optim.Adam(model.parameters(),
+                       lr=args.lr, weight_decay=args.weight_decay)
 
 if args.cuda:
     model.cuda()
@@ -93,11 +80,12 @@ def train(epoch):
 
     loss_val = F.nll_loss(output[idx_val], labels[idx_val])
     acc_val = accuracy(output[idx_val], labels[idx_val])
-    print(
-        'Epoch: {:04d}'.format(epoch + 1), 'loss_train: {:.4f}'.format(
-            loss_train.data[0]), 'acc_train: {:.4f}'.format(acc_train.data[0]),
-        'loss_val: {:.4f}'.format(loss_val.data[0]), 'acc_val: {:.4f}'.format(
-            acc_val.data[0]), 'time: {:.4f}s'.format(time.time() - t))
+    print('Epoch: {:04d}'.format(epoch+1),
+          'loss_train: {:.4f}'.format(loss_train.data[0]),
+          'acc_train: {:.4f}'.format(acc_train.data[0]),
+          'loss_val: {:.4f}'.format(loss_val.data[0]),
+          'acc_val: {:.4f}'.format(acc_val.data[0]),
+          'time: {:.4f}s'.format(time.time() - t))
 
 
 def test():
@@ -105,7 +93,8 @@ def test():
     output = model(features, adj)
     loss_test = F.nll_loss(output[idx_test], labels[idx_test])
     acc_test = accuracy(output[idx_test], labels[idx_test])
-    print("Test set results:", "loss= {:.4f}".format(loss_test.data[0]),
+    print("Test set results:",
+          "loss= {:.4f}".format(loss_test.data[0]),
           "accuracy= {:.4f}".format(acc_test.data[0]))
 
 
