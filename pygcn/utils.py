c91f69c7d0c7fbd1a8fd2bafde9d1cb48b290c06
Thomas Kipf <tkipf@users.noreply.github.com>
2018-08-05 15:50:59 +0100

    Style fix

--- pygcn/utils.py
+++ pygcn/utils.py
@@ -73,7 +73,8 @@ def accuracy(output, labels):
 def sparse_mx_to_torch_sparse_tensor(sparse_mx):
     """Convert a scipy sparse matrix to a torch sparse tensor."""
     sparse_mx = sparse_mx.tocoo().astype(np.float32)
-    indices = torch.from_numpy(np.vstack((sparse_mx.row, sparse_mx.col)).astype(np.int64))
+    indices = torch.from_numpy(
+        np.vstack((sparse_mx.row, sparse_mx.col)).astype(np.int64))
     values = torch.from_numpy(sparse_mx.data)
     shape = torch.Size(sparse_mx.shape)
     return torch.sparse.FloatTensor(indices, values, shape)
